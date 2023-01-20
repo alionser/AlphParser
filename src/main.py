1a94e56ac37bcabe1ee0a220f2285f51969ee159
alionser <smirnoviksa@gmail.com>
2023-01-20 17:17:56 +0500

    Сделал получение информациии об одном товаре (в ввиде словаря) по ссылке на него

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
+from pprint import pprint
+
 from parser import *
 
-print(get_product_links("subwoofers"))
-
+# print(get_product_links("subwoofers"))
+pprint(get_product_info("https://alphardaudio.ru/products/subwoofers/machete-mf-12r-d2-d4"))
--- src/parser.py
+++ src/parser.py
@@ -1,5 +1,5 @@
 from requests import get
-from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup
 
 BASE_ROUTE: str = "https://alphardaudio.ru"
 
@@ -7,7 +7,21 @@ BASE_ROUTE: str = "https://alphardaudio.ru"
 def get_product_links(category: str) -> list[str]:
     page = get(f"{BASE_ROUTE}/products/{category}")
     print(page.status_code)
-    link_elements = bs(page.text, "lxml").findAll("a", "product_list__button_more")
+    link_elements = BeautifulSoup(page.text, "lxml").findAll("a", "product_list__button_more")
     links = [BASE_ROUTE + el.get("href") for el in link_elements]
     print(len(links))
     return links
+
+
+def get_product_info(product_link: str):
+    product_page = get(product_link)
+    print(product_page.status_code)
+
+    product_info = dict()  # TODO: можно данные о товаре вынести в структуру
+    table = BeautifulSoup(product_page.text, "lxml").find("table", "table")
+    rows = table.findAll("tr")
+    for row in rows:
+        cells = row.findAll("td")
+        product_info[cells[0].getText()] = cells[1].getText().strip()
+    return product_info
+
